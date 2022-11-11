7fd481ddc4db8538b68e11b686d28d86eea4d3d6
serraguti <pacoserranox@gmail.com>
2022-11-11 14:26:12 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimerComponent } 
+  from 'src/components/primercomponent/primer.component';
+import { HooksAngular } from 
+  'src/components/hooksangular/hooksangular.component';
+import { DeportesComponent } from 'src/components/deportes/deportes.component';
 
+import { FormsModule } from '@angular/forms';
+import { FormularioBindingComponent } from 'src/components/formulariobinding/formulariobinding.component';
+import { SumarNumerosComponent } from 'src/components/sumarnumeros/sumarnumeros.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponent,
+    HooksAngular,
+    DeportesComponent,
+    FormularioBindingComponent,
+    SumarNumerosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/deportes/deportes.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "app-deportes",
+    templateUrl: "./deportes.component.html",
+    styleUrls: ["./deportes.component.css"]
+})
+
+export class DeportesComponent {
+    public sports: Array<string>;
+    public numeros: Array<number>;
+
+    constructor() {
+        this.numeros = [5, 6, 9, 12, 14, 53, 22];
+        this.sports = ["Petanca", "Curling", "Canicas", "Futbol", "Padel"];
+    }
+}
